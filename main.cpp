152c156befe14e8b0333a861f3dfd6976c4725ce
Xuan Tam Trinh <trinh@post.cz>
2022-04-06 23:58:10 +0200

    refactor sequential solution

--- main.cpp
+++ main.cpp
@@ -3,9 +3,11 @@
 //
 
 #include <iostream>
-#include <vector>
 #include <set>
+#include <vector>
 #include <chrono>
+#include <algorithm>
+#include <optional>
 
 enum VertexColor {
     UNASSIGNED,
@@ -18,71 +20,117 @@ struct Edge {
     int v2;
     int weight;
 
-    Edge(int v1, int v2, int weight) : weight(weight) {
-        if (v1 < v2) {
-            this->v1 = v1;
-            this->v2 = v2;
-        } else {
-            this->v1 = v2;
-            this->v2 = v1;
+    Edge(int v1, int v2, int weight) : v1(v1), v2(v2), weight(weight) {}
+
+    static bool cmp(const Edge& lhs, const Edge& rhs) {
+        if(lhs.v1 == rhs.v1) {
+            return lhs.weight >= rhs.weight;
         }
-    }
-
-    bool operator<(const Edge &rhs) const {
-        if (weight < rhs.weight)
-            return false;
-        if (rhs.weight < weight)
-            return true;
-        if (v1 < rhs.v1)
-            return true;
-        if (rhs.v1 < v1)
-            return false;
-        if (v2 < rhs.v2)
-            return true;
-        if (rhs.v2 < v2)
-            return false;
-        return false;
-
+        return lhs.v1 >= rhs.v1;
     }
 };
 
 struct Configuration {
     std::vector<VertexColor> coloring;
-    std::set<Edge> addedEdges;
+    std::vector<int> edges;
+   
+    Configuration(std::vector<VertexColor> coloring, std::vector<int> edges) : coloring(coloring), edges(edges) {}
 
-    Configuration(
-            const std::vector<VertexColor> &coloring,
-            const std::set<Edge> &addedEdges
-    ) : coloring(coloring), addedEdges(addedEdges) {}
-
-    bool operator<(const Configuration &rhs) const {
-        if (coloring < rhs.coloring)
-            return true;
-        if (rhs.coloring < coloring)
-            return false;
-        return addedEdges < rhs.addedEdges;
-    }
+    Configuration(std::vector<VertexColor> coloring) : coloring(coloring) {
+        this->edges = std::vector<int>();
+    }    
 };
 
-void printSolution(const int &bestValue, const int &recursionCnt, const std::set<Configuration> &results, const std::chrono::duration<double> time) {
+struct State {
+    std::vector<Edge> edges;
+    Configuration configuration;
+    int remainingWeight;
+    int currentWeight;
+    int index;
+
+    State(
+        std::vector<Edge> edges
+    ) : edges(edges), 
+        configuration(Configuration(std::vector<VertexColor>(edges.size(), VertexColor::UNASSIGNED))),
+        remainingWeight(0),
+        currentWeight(0),
+        index(0) {
+        for (const auto& edge : edges) {
+            this->remainingWeight += edge.weight;
+        }
+    }
+
+    State(
+        std::vector<Edge> edges,
+        Configuration configuration, 
+        int remainingWeight, 
+        int currentWeight, 
+        int index
+    ) : edges(edges),
+        configuration(configuration), 
+        remainingWeight(remainingWeight), 
+        currentWeight(currentWeight), 
+        index(index) {}
+
+    std::optional<State> next(VertexColor c1, VertexColor c2) const {
+        if ((size_t)(this->index) == this->edges.size()) {
+            return std::nullopt;
+        }
+
+        State newState = *this;
+        Edge current = edges[this->index];
+        newState.remainingWeight -= current.weight;
+        newState.index++;
+
+        if (c1 == VertexColor::UNASSIGNED  && c2 == VertexColor::UNASSIGNED) {
+            return newState;
+        }
+
+        if ((this->configuration.coloring[current.v1] == VertexColor::UNASSIGNED || this->configuration.coloring[current.v1] == c1)
+        && (this->configuration.coloring[current.v2] == VertexColor::UNASSIGNED || this->configuration.coloring[current.v2] == c2)) {
+            newState.currentWeight += current.weight;
+            newState.configuration.coloring[current.v1] = c1;
+            newState.configuration.coloring[current.v2] = c2;
+            newState.configuration.edges.emplace_back(this->index);
+            return newState;
+        }
+
+        return std::nullopt;
+    }    
+};
+
+void printSolution(
+    const std::vector<Edge>& edges,
+    const int &bestWeight, 
+    const int &recursionCnt, 
+    const std::vector<Configuration> &results, 
+    const std::chrono::duration<double> time
+) {
     std::cout << "Total time: " << time.count() << "s" << std::endl;
-    std::cout << "Max weight: " << bestValue << std::endl;
+    std::cout << "Max weight: " << bestWeight << std::endl;
     std::cout << "Recursion count: " << recursionCnt << std::endl;
-    std::cout << "Solutions: " << results.size() << std::endl;
 
     std::cout << std::endl;
-
+    
+    std::set<std::set<int>> uniqueSolutions;
     int solutionNo = 1;
     for (const auto &result: results) {
-        std::set<int> U;
-        std::set<int> W;
+        std::set<int> solutionEdges(result.edges.begin(), result.edges.end());
+        if (uniqueSolutions.find(solutionEdges) == uniqueSolutions.end()) {
+            uniqueSolutions.insert(solutionEdges);
+        } else {
+            continue;
+        }
+
+        std::vector<int> U;
+        std::vector<int> W;
 
         for (size_t j = 0; j < result.coloring.size(); j++) {
             if (result.coloring[j] == VertexColor::RED) {
-                U.insert(j);
+                U.emplace_back(j);
             }
             if (result.coloring[j] == VertexColor::BLUE) {
-                W.insert(j);
+                W.emplace_back(j);
             }
         }
 
@@ -101,8 +149,8 @@ void printSolution(const int &bestValue, const int &recursionCnt, const std::set
         std::cout << "}" << std::endl;
 
         std::cout << "E: { ";
-        for (const auto &e: result.addedEdges) {
-            std::cout << "(" << e.v1 << ", " << e.v2 << ") ";
+        for (const auto &e: result.edges) {
+            std::cout << "(" << edges[e].v1 << ", " << edges[e].v2 << ") ";
         }
         std::cout << "}" << std::endl;
 
@@ -111,128 +159,72 @@ void printSolution(const int &bestValue, const int &recursionCnt, const std::set
     }
 }
 
-bool isColorAssignmentValid(const Edge &edge, VertexColor c1, VertexColor c2, const Configuration &configuration) {
-    int v1 = edge.v1;
-    int v2 = edge.v2;
-
-    return (configuration.coloring[v1] == VertexColor::UNASSIGNED || configuration.coloring[v1] == c1) &&
-           (configuration.coloring[v2] == VertexColor::UNASSIGNED || configuration.coloring[v2] == c2);
-}
-
-void solve(
-        int &recursionCnt,
-        int &bestValue,
-        std::set<Configuration> &results,
-        Configuration current,
-        std::vector<std::set<Edge>> adjacencyMatrix,
-        std::set<Edge> openedEdges,
-        int remainingValue,
-        int currentValue
-) {
+void solve(int &recursionCnt, int &bestWeight, std::vector<Configuration> &results, const State& state) {
     recursionCnt++;
-
-    // Better value was found.
-    if (currentValue > bestValue) {
-        bestValue = currentValue;
-        results.clear();
-    }
+    
+    if (state.currentWeight >= bestWeight) {
+        #pragma omp critical
+        {
+            // Better value was found.
+            if (state.currentWeight > bestWeight) {
+                bestWeight = state.currentWeight;
+                results.clear();
+            }
             
-    // If current value is the best, mark added edges as solution.
-    if (currentValue == bestValue) {
-        results.insert(current);
+            // If current value is the best, mark added edges as solution.
+            if (state.currentWeight == bestWeight) {
+                results.push_back(state.configuration);
+            }
+        }
     }
     
     // Better solution can't be found.
-    if (currentValue + remainingValue < bestValue) { return; }
+    if (state.currentWeight + state.remainingWeight < bestWeight) { return; }
 
     // No opened edges are left.
-    if (openedEdges.empty()) { return; }
+    if ((size_t)(state.index) == state.edges.size()) { return; }
 
-    // Remove the heaviest edge from opened edges.
-    Edge heaviestEdge = *openedEdges.begin();
-    openedEdges.erase(openedEdges.begin());
-    adjacencyMatrix[heaviestEdge.v1].erase(heaviestEdge);
-    adjacencyMatrix[heaviestEdge.v2].erase(heaviestEdge);
-    remainingValue -= heaviestEdge.weight;
-
-    // Color vertices.
-    VertexColor c1 = VertexColor::UNASSIGNED;
-    VertexColor c2 = VertexColor::UNASSIGNED;
-    if (isColorAssignmentValid(heaviestEdge, VertexColor::RED, VertexColor::BLUE, current)) {
-        c1 = VertexColor::RED;
-        c2 = VertexColor::BLUE;
-    } else if (isColorAssignmentValid(heaviestEdge, VertexColor::BLUE, VertexColor::RED, current)) {
-        c1 = VertexColor::BLUE;
-        c2 = VertexColor::RED;
-    }
-    if (c1 != VertexColor::UNASSIGNED && c2 != VertexColor::UNASSIGNED) {
-        Configuration newConfiguration = Configuration(current.coloring, current.addedEdges);
-        newConfiguration.coloring[heaviestEdge.v1] = c1;
-        newConfiguration.coloring[heaviestEdge.v2] = c2;
-        newConfiguration.addedEdges.insert(heaviestEdge);
-        std::vector<std::set<Edge>> newAdjacencyMatrix = adjacencyMatrix;
-        std::set<Edge> newOpenedEdges = openedEdges;
-
-        for (auto e: adjacencyMatrix[heaviestEdge.v1]) {
-            newAdjacencyMatrix[e.v1].erase(e);
-            newAdjacencyMatrix[e.v2].erase(e);
-            newOpenedEdges.insert(e);
-        }
-
-        for (auto e: adjacencyMatrix[heaviestEdge.v2]) {
-            newAdjacencyMatrix[e.v1].erase(e);
-            newAdjacencyMatrix[e.v2].erase(e);
-            newOpenedEdges.insert(e);
-        }
-
-        solve(recursionCnt, bestValue, results, newConfiguration, newAdjacencyMatrix, newOpenedEdges, remainingValue, currentValue + heaviestEdge.weight);
+    // Add edge to the solution if valid coloring exists.
+    std::optional<State> blueRedState = state.next(VertexColor::BLUE, VertexColor::RED);
+    std::optional<State> redBlueState = state.next(VertexColor::RED, VertexColor::BLUE);
+    if (blueRedState) {
+        solve(recursionCnt, bestWeight, results, *blueRedState);
+    } 
+    if (redBlueState) {
+        solve(recursionCnt, bestWeight, results, *redBlueState);
     }
 
     // Do not add edge to the solution.
-    solve(recursionCnt, bestValue, results, current, adjacencyMatrix, openedEdges, remainingValue, currentValue);
+    std::optional<State> edgeNotAddedState = state.next(VertexColor::UNASSIGNED, VertexColor::UNASSIGNED);
+    if (edgeNotAddedState) {
+        solve(recursionCnt, bestWeight, results, *edgeNotAddedState);
+    }
+
 }
 
 int main(int argc, char *argv[]) {
     int vertexCnt = 0;
+    int bestWeight = 0;
     int recursionCnt = 0;
-    int bestValue = 0;
-    int totalWeight = 0;
-    std::set<Configuration> results;
-    std::set<Edge> openedEdges;
-    Edge heaviestEdge = Edge(-1, -1, -1);
-
+    std::vector<Edge> edges;
+    std::vector<Configuration> results;
     std::cin >> vertexCnt;
-    std::vector<std::set<Edge>> adjacencyMatrix = std::vector<std::set<Edge>>(vertexCnt, std::set<Edge>());
-
     for (int i = 0; i < vertexCnt; ++i) {
         for (int j = 0; j < vertexCnt; ++j) {
             int weight;
             std::cin >> weight;
             if (weight != 0 && j >= i) {
-                Edge edge(i, j, weight);
-                adjacencyMatrix[i].insert(edge);
-                adjacencyMatrix[j].insert(edge);
-                if (weight > heaviestEdge.weight) {
-                    heaviestEdge = edge;
-                }
-                totalWeight += weight;
+                edges.emplace_back(i, j, weight);
             }
         }
     }
-
-    std::vector<VertexColor> coloring = std::vector<VertexColor>(vertexCnt, VertexColor::UNASSIGNED);
-    std::set<Edge> addedEdges;
-
-    openedEdges = adjacencyMatrix[heaviestEdge.v1];
-    coloring[heaviestEdge.v1] = VertexColor::RED;
-    adjacencyMatrix[heaviestEdge.v1].erase(heaviestEdge);
-    adjacencyMatrix[heaviestEdge.v2].erase(heaviestEdge);
+    std::sort(edges.begin(), edges.end(), Edge::cmp);
 
     auto startTime = std::chrono::high_resolution_clock::now();
-
-    solve(recursionCnt, bestValue, results, Configuration(coloring, addedEdges), adjacencyMatrix, openedEdges, totalWeight, 0);
-
+    solve(recursionCnt, bestWeight, results, State(edges));
     auto endTime = std::chrono::high_resolution_clock::now();
 
-    printSolution(bestValue, recursionCnt, results, endTime - startTime);
+    printSolution(edges, bestWeight, recursionCnt, results, endTime - startTime);
+
+    return 0;
 }
